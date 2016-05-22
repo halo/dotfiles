fb9f1e1acf72e43405dab92983b70a9b9d0287b1
halo <github@posteo.org>
2016-05-22 19:21:21 +0200

    [trackpad] really tap to click

--- macos/os/trackpad.bash
+++ macos/os/trackpad.bash
@@ -8,3 +8,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightC
 
 # System Preferences -> Trackpad -> Tap to click -> Yes
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
