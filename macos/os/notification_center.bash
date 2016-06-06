f0e9f21c2c12de632d818dc5ec2c134b8e5ccb0b
halo <github@posteo.org>
2016-06-06 22:24:51 +0200

    Move User Pictures in place (requires sudo)

--- macos/os/notification_center.bash
+++ macos/os/notification_center.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Notification Center -> Begone
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist &> /dev/null
