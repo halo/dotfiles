b5953a75a6860e98104ef818a58b667d715a8396
halo <github@posteo.org>
2016-06-10 15:00:20 +0200

    [screensaver] Use Plistbuddy instead

--- macos/os/screen_saver.bash
+++ macos/os/screen_saver.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Finding the newest relevant preference file
-filename="$(ls -t ~/Library/Preferences/ByHost/com.apple.screensaver.*.plist | head -1)"
+filename="$(ls -t $HOME/Library/Preferences/ByHost/com.apple.screensaver.*.plist | head -1)"
 
 # System Settings -> Screen Saver -> Flurry
-defaults write $filename moduleDict -dict moduleName Flurry path /System/Library/Screen\ Savers/Flurry.saver type 0
+/usr/libexec/PlistBuddy -c "Delete :moduleDict:moduleName" $filename &> /dev/null
+/usr/libexec/PlistBuddy -c "Delete :moduleDict:type" $filename &> /dev/null
+/usr/libexec/PlistBuddy -c "Delete :moduleDict:path" $filename &> /dev/null
+/usr/libexec/PlistBuddy -c "Add :moduleDict:moduleName string Flurry" $filename &> /dev/null
+/usr/libexec/PlistBuddy -c "Add :moduleDict:type int 0" $filename &> /dev/null
+/usr/libexec/PlistBuddy -c "Add :moduleDict:path string '/System/Library/Screen Savers/Flurry.saver'" $filename &> /dev/null
 
 # System Settings -> Screen Saver -> Start after -> Never
 defaults write $filename idleTime -int 0
