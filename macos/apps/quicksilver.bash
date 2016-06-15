9f35e7144c567f6522b35e44b293dc36a871229f
halo <github@posteo.org>
2016-06-15 22:25:04 +0200

    [quicksilver] Fixed the keycode command

--- macos/apps/quicksilver.bash
+++ macos/apps/quicksilver.bash
@@ -6,4 +6,7 @@ defaults write com.blacktree.Quicksilver QSAgreementAccepted -bool true
 defaults write com.blacktree.Quicksilver "Setup Assistant Completed" -bool true
 
 # Use CMD+. to activate
-defaults write com.blacktree.Quicksilver QSActivationHotKey -dict keyCode 47 modifiers 1048848
+defaults write com.blacktree.Quicksilver QSActivationHotKey -dict keyCode -int 47 modifiers -int 1048848
+
+# Nicer layout for the main window
+defaults write com.blacktree.Quicksilver QSCommandInterfaceControllers QSBezelInterfaceController
