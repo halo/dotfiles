42adf830299ecbfdeffeb8f328d4f40569e7fc41
halo <github@posteo.org>
2016-06-15 21:01:28 +0200

    [screensaver] I think it's finally working Drastically simplified...

--- macos/os/screen_saver.bash
+++ macos/os/screen_saver.bash
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Finding the newest relevant preference file
-filename="$(ls -t $HOME/Library/Preferences/ByHost/com.apple.screensaver.*.plist | head -1)"
+defaults -currentHost write com.apple.screensaver moduleDict -dict \
+  moduleName Flurry \
+  path "/System/Library/Screen Savers/Flurry.saver" \
+  type -int 0 \
 
-# System Settings -> Screen Saver -> Flurry
-/usr/libexec/PlistBuddy -c "Delete :moduleDict:moduleName" $filename &> /dev/null
-/usr/libexec/PlistBuddy -c "Delete :moduleDict:type" $filename &> /dev/null
-/usr/libexec/PlistBuddy -c "Delete :moduleDict:path" $filename &> /dev/null
-/usr/libexec/PlistBuddy -c "Add :moduleDict:moduleName string Flurry" $filename &> /dev/null
-/usr/libexec/PlistBuddy -c "Add :moduleDict:type int 0" $filename &> /dev/null
-/usr/libexec/PlistBuddy -c "Add :moduleDict:path string '/System/Library/Screen Savers/Flurry.saver'" $filename &> /dev/null
-
-# System Settings -> Screen Saver -> Start after -> Never
-defaults write $filename idleTime -int 0
-
+defaults -currentHost write com.apple.screensaver idleTime -int 0
