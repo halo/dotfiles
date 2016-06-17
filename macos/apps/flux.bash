b3d3e9564e3e0811b6d1499561b8694ff856c987
halo <github@posteo.org>
2016-06-17 14:04:55 +0200

    [flux] Added some location to avoid a popup

--- macos/apps/flux.bash
+++ macos/apps/flux.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Bedtime color
-defaults write org.herf.Flux nightColorTemp -int 4300
+# Wake up time 7:00
+defaults write org.herf.Flux wakeTime -int 420
 
 # Sunset color
 defaults write org.herf.Flux lateColorTemp -int 3000
 
-# Wake up time
-defaults write org.herf.Flux wakeTime -int 420
+# Bedtime color
+defaults write org.herf.Flux nightColorTemp -int 4300
+
+# Geolocation
+defaults write org.herf.Flux location "64.963051,-19.020835"
+defaults write org.herf.Flux locationTextField Iceland
+defaults write org.herf.Flux locationType L
