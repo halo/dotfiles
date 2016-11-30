92ae94136bfc2771ac5659dca0199b643e51aa76
halo <github@posteo.org>
2016-11-30 13:22:11 +0100

    TextMate 2 is out of beta

--- macos/apps/textmate.bash
+++ macos/apps/textmate.bash
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 # Don't mess with my Tabs
-defaults write com.macromates.TextMate.preview disableTabReordering -bool true
+defaults write com.macromates.TextMate disableTabReordering -bool true
 
 # Don't jump around in File Browser
-defaults write com.macromates.TextMate.preview autoRevealFile -bool false
+defaults write com.macromates.TextMate autoRevealFile -bool false
 
 # Always wrap around search
-defaults write com.macromates.TextMate.preview findWrapAround -bool true
+defaults write com.macromates.TextMate findWrapAround -bool true
 
 # Tabs all the way through
-defaults write com.macromates.TextMate.preview tabsAboveDocument -bool false
+defaults write com.macromates.TextMate tabsAboveDocument -bool false
 
 # Allow rather narrow tabs
-defaults write com.macromates.TextMate.preview tabItemMinWidth -int 50
+defaults write com.macromates.TextMate tabItemMinWidth -int 50
 
 # Truncate the filename in the tabs
-defaults write com.macromates.TextMate.preview tabItemLineBreakStyle -int 2
+defaults write com.macromates.TextMate tabItemLineBreakStyle -int 2
 
 # Show file browser to the right
-defaults write com.macromates.TextMate.preview fileBrowserPlacement right
+defaults write com.macromates.TextMate fileBrowserPlacement right
 
 # Always show the tab bar
-defaults write com.macromates.TextMate.preview disableTabBarCollapsing -bool true
+defaults write com.macromates.TextMate disableTabBarCollapsing -bool true
 
 # Set Environment variables
-defaults write com.macromates.TextMate.preview environmentVariables -array
+defaults write com.macromates.TextMate environmentVariables -array
 
-defaults write com.macromates.TextMate.preview environmentVariables -array-add \
+defaults write com.macromates.TextMate environmentVariables -array-add \
   "<dict>
     <key>enabled</key><true/>
     <key>name</key><string>TM_RUBY</string>
     <key>value</key><string>$(which ruby)</string>
   </dict>"
 
-defaults write com.macromates.TextMate.preview environmentVariables -array-add \
+defaults write com.macromates.TextMate environmentVariables -array-add \
   "<dict>
     <key>enabled</key><true/>
     <key>name</key><string>RBENV_VERSION</string>
     <key>value</key><string>2.3.1</string>
   </dict>"
 
-defaults write com.macromates.TextMate.preview environmentVariables -array-add \
+defaults write com.macromates.TextMate environmentVariables -array-add \
   "<dict>
     <key>enabled</key><true/>
     <key>name</key><string>TM_NODE</string>
