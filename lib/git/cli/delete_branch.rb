bd474c96ff42477403831cf24376b60f77d9574e
halo <github@posteo.org>
2019-06-14 12:57:10 +0200

    Fix variable name typo

--- lib/git/cli/delete_branch.rb
+++ lib/git/cli/delete_branch.rb
@@ -2,7 +2,7 @@ module Git
   module CLI
     module DeleteBranch
       def self.call(name)
-        puts "Deleting branch #{branch}"
+        puts "Deleting branch #{name}"
         return if Git::Runtime.dry_mode?
 
         system "git branch -D #{name} &> /dev/null"
