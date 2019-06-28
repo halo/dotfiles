ab3277936062c704447b4bebdc8227253677dafe
halo <github@posteo.org>
2019-06-28 18:30:20 +0200

    [keyboard] Fix shortcuts properly

--- lib/macos/customize/keyboard.rb
+++ lib/macos/customize/keyboard.rb
@@ -1,33 +1,48 @@
 module MacOS
   module Customize
     class Keyboard
+      #  @ = command
+      #  ^ = control
+      #  ~ = option
+      #  $ = shift
+      #  \0 = NULL
       def call
-        disable caption: 'Hide Others'
-        disable caption: 'Enter Full Screen'
+        set_shortcut caption: 'Hide Others', keycode: '\0'
+        set_shortcut caption: 'Enter Full Screen', keycode: '\0'
+        set_shortcut caption: 'Minimize', keycode: '\0'
+
+        set_shortcut caption: 'Next Tab', keycode: "~@→"
+        set_shortcut caption: 'Show Next Tab', keycode: "~@→"
+        set_shortcut caption: 'Select Next Tab', keycode: "~@→"
+
+        set_shortcut caption: 'Previous Tab', keycode: "~@←"
+        set_shortcut caption: 'Show Previous Tab', keycode: "~@←"
+        set_shortcut caption: 'Select Previous Tab', keycode: "~@←"
 
         system 'killall Finder' if @restart_finder && Runtime.apply_mode?
       end
 
       private
 
-      def disable(caption:)
-        check = Check.new good: %("#{caption}" shortcut is deactivated)
+      def set_shortcut(caption:, keycode:)
+        readable_keycode = keycode == '\0' ? :deactivated : keycode
+        check = Check.new good: %("#{caption}" shortcut is #{readable_keycode})
+
         setting = OS::Defaults::Dict.new(key: 'NSUserKeyEquivalents')
-
-        expected_escaped_output = %("#{caption}" = "#{suffix_separator.gsub('\\', '\\\\\\')}")
-
-        if setting.match(expected_string: expected_escaped_output)
+        if setting.match(expected_string: expected_output(caption: caption, keycode: keycode))
           check.pass!
         else
-          setting.add! key: caption, value: suffix_separator
+          setting.add! key: caption, value: keycode
           check.fixed!
           @restart_finder = true
         end
       end
 
-      # Two additional backslashes for ruby string escaping.
-      def suffix_separator
-        '@~^$\\\\U00b4'
+      def expected_output(caption:, keycode:)
+        return '' if keycode == '\0'
+        keycodes = keycode.split('')
+        keycodes[-1] = "\\\\U#{keycodes.last.ord.to_s(16)}"
+        %("#{caption}" = "#{keycodes.join}")
       end
     end
   end
